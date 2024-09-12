9db0708e3805a09db12ae79631985c3671e838b4
Jorge Henrique <jorgehenriquelessaoliveira@gmail.com>
2024-09-12 18:04:57 -0300

    professor funcionando capengo

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class MyTask(db.Model):
+class Tarefa(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(100), nullable = False)
-    complete = db.Column(db.Integer, default = 0)
-    created = db.Column(db.DateTime, default = datetime.utcnow)
+    conteudo = db.Column(db.String(100), nullable = False)
+    feito = db.Column(db.Integer, default = 0)
+    criado = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f"Task {self.id}"
+        return f"Tarefa {self.id}"
 
 with app.app_context():
     db.create_all()
@@ -22,24 +22,24 @@ with app.app_context():
 @app.route("/", methods = ["POST","GET"])
 def index():
     if request.method == "POST":
-        current_task = request.form['content']
-        new_task = MyTask(content=current_task)
+        conteudo_tarefa = request.form.get('conteudo')
+        nova_tarefa = Tarefa(conteudo=conteudo_tarefa)
         try:
-            db.session.add(new_task)
+            db.session.add(nova_tarefa)
             db.session.commit()
             return redirect("/")
         except Exception as e:
             print(f"ERROR: {e}")
             return f"ERROR: {e}"
     else:
-        tasks = MyTask.query.order_by(MyTask.created).all()
-        return render_template("index.html", tasks = tasks)
+        tarefas = Tarefa.query.order_by(Tarefa.criado).all()
+        return render_template("professor.html", tarefas = tarefas)
 
 @app.route("/delete/<int:id>")
 def delete(id:int):
-    delete_task = MyTask.query.get_or_404(id)
+    deletar = Tarefa.query.get_or_404(id)
     try:
-        db.session.delete(delete_task)
+        db.session.delete(deletar)
         db.session.commit()
         return redirect("/")
     except Exception as e:
@@ -47,9 +47,9 @@ def delete(id:int):
 
 @app.route("/edit/<int:id>", methods= ["GET","POST"])
 def edit(id:int):
-    task = MyTask.query.get_or_404(id)
+    task = Tarefa.query.get_or_404(id)
     if request.method == "POST":
-        task.content = request.form['content']
+        task.conteudo = request.form['conteudo']
         try:
             db.session.commit()
             return redirect("/")
